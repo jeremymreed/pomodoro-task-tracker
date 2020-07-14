ef7a818b31015ec4eb4cc61b54344525ea001a31
Jeremy Reed <reeje76@gmail.com>
2020-07-14 14:18:05 -0400

    Add React components.

---
+++ components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>💖 Hello World!</h1>
+        <p>Welcome to your Electron application.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
