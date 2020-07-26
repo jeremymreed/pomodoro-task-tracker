94165ccc4bb75594a00313c276732d04028777e2
Jeremy Reed <reeje76@gmail.com>
2020-07-26 16:35:03 -0400

    Fix paths.
    EditTaskDialog stuff has been moved.

--- src/components/EditTaskDialog/dialog-index.jsx
+++ src/components/EditTaskDialog/dialog-index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import EditTaskDialog from './components/EditTaskDialog';
+import EditTaskDialog from './components/EditTaskDialog/EditTaskDialog';
 
 ReactDOM.render(
   <EditTaskDialog />,
