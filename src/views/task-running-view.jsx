07d27262a06f2a4c91797df2da7f57b6a9377cd3
Jeremy Reed <reeje76@gmail.com>
2020-07-27 23:03:12 -0400

    Implement a bunch of timer related handlers.
    Handle timer experiation.
    
    Create _startTimer and _stopTimer code to wrap around setState calls.
    
    Implement Pause and Resume.
    Implement Done.
    Implement taskDone.
    
    Hook in handlers on Pause, Resume, and Done buttons.
    
    Whew!
    
    This should complete UI for the task running view and timer.
    For now. :D

--- src/views/task-running-view.jsx
+++ src/views/task-running-view.jsx
@@ -6,36 +6,71 @@ class TaskRunningView extends React.Component {
   constructor(props) {
     super(props);
 
+    this.handleTimerExpiration = this.handleTimerExpiration.bind(this);
+
     this.state = {
       shouldRun: true,
     }
   }
 
-  handleTimerExpiration() {
+  _startTimer() {
+    this.setState({shouldRun: true});
+  }
+
+  _stopTimer() {
     this.setState({shouldRun: false});
   }
 
+  // Timer tells us it has expired.
+  handleTimerExpiration() {
+    this._stopTimer();
+  }
+
+  // User wants to pause the timer.
+  handlePause(event) {
+    event.preventDefault();
+
+    this._stopTimer();
+  }
+
+  // User wants to resume the timer.
+  handleResume(event) {
+    event.preventDefault();
+
+    this._startTimer();
+  }
+
+  // User wants to stop working on this task, and return to the task list.
+  // This does not set the task as being 'done'.
   stopTask(event) {
     event.preventDefault();
 
+    this._stopTimer();
     this.props.stopTask();
+  }
+
+  taskDone(event) {
+    event.preventDefault();
+
+    this._stopTimer();
+    this.props.taskDone();
   }
 
   render() {
     let pauseResumeButton = '';
     if (this.state.shouldRun) {
-      pauseResumeButton = <button>Pause</button>;
+      pauseResumeButton = <button onClick={(e) => this.handlePause(e)}>Pause</button>;
     } else {
-      pauseResumeButton = <button>Resume</button>;
+      pauseResumeButton = <button onClick={(e) => this.handleResume(e)}>Resume</button>;
     }
     return (
       <div>
         <p>This task is running hot!</p>
-        <Timer shouldRun={ this.state.shouldRun }/>
+        <Timer shouldRun={ this.state.shouldRun } handleTimerExpiration={ this.handleTimerExpiration }/>
         <p>Task Name: { this.props.task._name }</p>
         <p>Task Description: { this.props.task._description }</p>
         <p>{ pauseResumeButton }</p>
-        <p><button onClick={(e) => this.stopTask(e)}>Stop</button><button>Done</button></p>
+        <p><button onClick={(e) => this.stopTask(e)}>Stop</button><button onClick={(e) => this.taskDone(e)}>Done</button></p>
       </div>
     );
   }
@@ -43,7 +78,8 @@ class TaskRunningView extends React.Component {
 
 TaskRunningView.propTypes = {
   task: PropTypes.object,
-  stopTask: PropTypes.func
+  stopTask: PropTypes.func,
+  taskDone: PropTypes.func
 }
 
 export default TaskRunningView;
