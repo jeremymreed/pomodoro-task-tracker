14c254f1ad752c4f4f7be4f74e52935e97eb487d
Jeremy Reed <reeje76@gmail.com>
2020-08-10 17:07:49 -0400

    Add ViewTaskView

---
+++ src/views/view-task-view.jsx
@@ -0,0 +1,111 @@
+/*
+Copyright © 2020 Jeremy M. Reed
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/styles';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = () => ({
+  name: {
+    marginBottom: '5px'
+  },
+  description: {
+    marginTop: '5px',
+    marginBottom: '5px'
+  },
+  done: {
+    marginTop: '5px',
+    marginBottom: '5px'
+  },
+  exitButton: {
+    marginTop: '5px',
+  }
+});
+
+class ViewTaskView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: this.props.task.name,
+      description: this.props.task.description,
+      done: this.props.task.done
+    }
+  }
+
+  exit(event) {
+    event.preventDefault();
+
+    this.props.closeViewTaskView();
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Typography variant="h1" align="center">Task Editor</Typography>
+
+        <FormGroup>
+          <TextField
+            className="name"
+            label="Name"
+            defaultValue={this.state.name}
+            inputProps={{readOnly: true}}
+          />
+
+          <TextField
+            className="description"
+            label="Description"
+            multiline
+            rows={4}
+            defaultValue={ this.state.description }
+            inputProps={{readOnly: true}}
+          />
+
+          <FormControlLabel
+            className="done"
+            control= {<Checkbox
+              checked={ this.state.done }
+              color="primary"
+              inputProps={{ readOnly: true, 'aria-label': 'task done checkbox' }}
+            />}
+            label="Done"
+          />
+
+          <span>
+            <Button className={classes.exitButton} variant="outlined" color="primary" onClick={(e) => this.exit(e)}>Exit</Button>
+          </span>
+        </FormGroup>
+      </div>
+    );
+  }
+}
+
+ViewTaskView.propTypes = {
+  classes: PropTypes.object,
+  task: PropTypes.object,
+  closeViewTaskView: PropTypes.func
+}
+
+export default withStyles(styles)(ViewTaskView);
