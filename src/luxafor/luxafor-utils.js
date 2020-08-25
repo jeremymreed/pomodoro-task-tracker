ef7ec2a8417e4dcc70699c556fbf7b55537ce7b3
Jeremy Reed <reeje76@gmail.com>
2020-08-25 13:48:10 -0400

    Add color(), and active boolean.
    Gives us a way to check to be sure the flag is actually connected.
    When we implement luxafor methods, we check the active boolean, and do
    nothing if the luxafor device is not connected.
    
    Also implement color()

--- src/luxafor/luxafor-utils.js
+++ src/luxafor/luxafor-utils.js
@@ -3,6 +3,7 @@ import {Luxafor} from 'node-luxafor2';
 class LuxaforUtils {
   constructor() {
     this.luxafor = null;
+    this.active = false;
   }
 
   // Try to start Luxafor, if init fails, we assume we don't have a luxafor device attached.
@@ -10,9 +11,17 @@ class LuxaforUtils {
     try {
       this.luxafor = new Luxafor();
       this.luxafor.init();
+      this.active = true;
     } catch (error) {
       console.log('Failed to init luxafor, disabling!')
       this.luxafor = null;
+      this.active = false;
+    }
+  }
+
+  color(ledGroup, red, green, blue) {
+    if (this.active) {
+      this.luxafor.color(ledGroup, red, green, blue);
     }
   }
 }
