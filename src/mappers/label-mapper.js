a30b62f24f71b619c5499a8669f1d15e887e1ec8
Jeremy Reed <reeje76@gmail.com>
2020-09-02 11:22:02 -0400

    Create LabelMapper
    Map label raw data to Label data model objects.

---
+++ src/mappers/label-mapper.js
@@ -0,0 +1,28 @@
+/*
+Copyright © 2020 Jeremy M. Reed
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+import Label from '../data-models/label';
+
+// data is raw doc from PouchDB / CouchDB.
+class LabelMapper {
+  static mapDataToLabel(data) {
+    return new Label(data._id, data._rev, data.name, data.description, data.label);
+  }
+}
+
+export default LabelMapper;
