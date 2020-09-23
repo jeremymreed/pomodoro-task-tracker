a5e2be38eec820d00d0de5dfe7283221062fdacb
Jeremy Reed <reeje76@gmail.com>
2020-09-23 16:20:54 -0400

    Run eslint --init
    Configure to check for errors and style.
    Selected airbnb style.
    Selected react, with typescript.
    Selected browser and node environments.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,25 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "browser": true,
-        "es2020": true,
-        "mocha": true
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    },
-    "rules": {
-    }
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
 };
